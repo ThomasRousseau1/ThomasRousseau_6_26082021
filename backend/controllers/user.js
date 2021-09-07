15a10f56e26f9ce82be93aaff494c9116338c0de
ThomasRousseau1 <thomas.rousseau500@outlook.fr>
2021-09-07 10:05:19 +0200

    Add comments

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -7,7 +7,7 @@ exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)//ajout du mdp du corps de la requête venant du frontend
         .then(hash => {
             const user = new User ({ 
-                email: req.body.email, 
+                email: req.body.email, //passe l'adresse fournie dans le corps de la requête
                 password: hash
             });
             user.save()//pour enregistrer l'User dans la base de données
@@ -24,7 +24,7 @@ exports.login = (req, res, next) => {
     console.dir(req.headers.authorization);
     User.findOne({ email: req.body.email })
         .then(user => {
-            if (!user) {
+            if (!user) {//Si l'email n'est pas bon, renvoie l'erreur suivante 
                 return res.status(401).json({ error: 'Utilisateur non trouvé !' });
             }
             bcrypt.compare(req.body.password, user.password)//Comparaison du mot de passe envoyé par la requête avec le hash de la base de données
@@ -35,9 +35,9 @@ exports.login = (req, res, next) => {
                     res.status(200).json({//Si la comparaison est valable, renvoi d'un userid et d'un token à l'utilisateur
                         userId: user._id,
                         token: jwt.sign(                    
-                               { userId: user._id },   
-                               'RANDOM_TOKEN_SECRET',
-                               { expiresIn: '24h'}                   
+                               { userId: user._id },//Données qu'on veut encoder dans le token(payload)   
+                               'RANDOM_TOKEN_SECRET',//Clef secrète pour l'encodage
+                               { expiresIn: '24h'}//Date d'expiration du token                   
                         )
                     });
                 })
