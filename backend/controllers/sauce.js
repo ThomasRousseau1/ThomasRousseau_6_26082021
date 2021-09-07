6827cede51f07cf5d90723bc742d5449ff1ddf61
ThomasRousseau1 <thomas.rousseau500@outlook.fr>
2021-09-07 10:04:04 +0200

    Mod modifySauce  controller for images

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 //Controller création de sauce
 exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);//extraction de l'objet JSON de sauce 
-    delete sauceObject._id
+    delete sauceObject._id;
     const sauce = new Sauce ({ 
         ...sauceObject,//Opérateur spread pour copier l'instance de Sauce et l'importer ici 
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`//Générer l'url de l'image: le protocole, le nom d'hôte /image/ et le nom de fichier
@@ -17,11 +17,18 @@ exports.createSauce = (req, res, next) => {
 
 //Controller pour modifier une sauce
 exports.modifySauce = (req, res, next) => {
-    const sauceObject = req.file ?//Si on trouve un fichier
-    { 
-        ...JSON.parse(req.body.sauce),//Récupération de la chaîne de caractères parsée en objet
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`//On modifie l'imageUrl
-    } : { ...req.body };//Sinon, on prend le corps de la requête
+    let sauceObject = {};//création objet vide
+    req.file ?//Si on trouve un fichier
+     (
+        Sauce.findOne({ _id: req.params.id })//récupération d'une seule sauce via l'id
+        .then((sauce) => {
+            const filename = sauce.imageUrl.split('/images/')[1]//récupération du fichier image de la sauce
+            fs.unlinkSync(`images/${filename}`)//
+        })
+        .catch(error => res.status(400).json({ error })),
+        sauceObject = {...JSON.parse(req.body.sauce),//Récupération de la chaîne de caractères parsée en objet
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,//On modifie l'imageUrl
+        }) : (sauceObject = {...req.body});//Sinon : , on prend le corps de la requête
     Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })//On prend sauceObject en modifiant son identifiant pour correspondre aux paramètres de req
     .then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
     .catch(error => res.status(400).json({ error }));
@@ -31,7 +38,7 @@ exports.modifySauce = (req, res, next) => {
 exports.deleteSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })//Trouver l'objet dans la base de données
     .then(sauce => {
-        const filename = sauce.imageUrl.split('/images')[1];//Pour récupérer le nom du fichier qui vient après /images/ et extraire le fichier à supprimer
+        const filename = sauce.imageUrl.split('/images/')[1];//Pour récupérer le nom du fichier qui vient après /images/ et extraire le fichier à supprimer
         fs.unlink(`images/${filename}`, () => {//On le supprime avec fs.unlink
             Sauce.deleteOne({ _id: req.params.id })//callback de fs.unlink, on supprimer la suppression de l'objet dans la base
             .then(() => res.status(200).json({ message: 'Sauce supprimée !'}))
